e67f06f60f765a24d6e8618c7c717f2816749694
matveyyuri <murin8102@gmail.com>
2026-09-21 20:16:51 +0300

    6

---
+++ 6.sql
@@ -0,0 +1,10 @@
+WITH raiting AS(
+        SELECT
+                 id,
+                 user_id,
+                 total,
+                 DENSE_RANK() OVER(PARTITION BY user_id, ORDER BY total DESC) AS dr
+)
+SELECT *
+FROM raiting
+WHERE dr = 1;
